927f8b8cca46912fcb2614536c2b5b9d0ce320f8
Anand Yerramsetty <anand.shopping@gmail.com>
2016-05-21 16:59:43 -0700

    Update Grade Application

--- src/Grade.java
+++ src/Grade.java
@@ -9,19 +9,19 @@ public class Grade {
 		String a= scanner.nextLine();
 		int b = Integer.parseInt(a); 
 		String grade="A grade";
-		if ((b<=100)&&(b>= 90))
+		if (b>= 90)
 			{
 			grade= "A Grade";
 			}
-		else if((b<=89) && (b >=80))
+		else if(b >=80)
 			{
 			grade= "B Grade";
 			}
-		else if((b<=79) && (b>=65))
+		else if(b>=65)
 			{
 			grade= "C Grade";
 			}
-		else if((b<=64) && (b>=50))
+		else if(b>=50)
 		{
 		grade= "D Grade";
 		}
